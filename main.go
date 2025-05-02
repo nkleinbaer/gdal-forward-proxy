354fa09c5385a031ca1d1747b76d86c1125d74fd
Nick Klein-Baer <nicholaskleinbaer@gmail.com>
2025-05-02 13:00:27 -0600

    Add support for `/vsis3`
    - Strip out headers that change between otherwise equivalent requests
    - Do not re-write Host header before making request to upstream

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"os"
 	"bytes"
@@ -25,16 +26,39 @@ func copyHeader(dst, src http.Header) {
 	}
 }
 
-func proxyHandler(w http.ResponseWriter, r *http.Request) {
-	// Serialize request to string (cache key)
-	var b = &bytes.Buffer{}
-	if err := r.Write(b); err != nil {
-		log.Fatal(err)
+
+type ProxyHandler struct {
+	stripHeaders []string
+}
+
+
+func (p ProxyHandler) GetKey(r http.Request) string {
+    // Ensure we don't modify the headers on the original request
+	copiedHeaders := make(http.Header, len(r.Header))
+    for k, v := range r.Header {
+        copiedHeaders[k] = append([]string(nil), v...)
+    }
+
+    r.Header = copiedHeaders
+    for _, header := range p.stripHeaders {
+        r.Header.Del(header)
+    }
+
+	key, err := httputil.DumpRequest(&r, true)
+	if err != nil {
+		panic(err)
 	}
+
+	return string(key)
+}
+
+func (p ProxyHandler) Handle (w http.ResponseWriter, r *http.Request) {
+	// Serialize request to string (cache key)
+	key := p.GetKey(*r)
 
 	// Hit the cache
 	var data []byte
-	if err := group.Get(r.Context(), b.String(), groupcache.AllocatingByteSliceSink(&data)); err != nil {
+	if err := group.Get(context.WithValue(r.Context(), "request", r), key, groupcache.AllocatingByteSliceSink(&data)); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -51,7 +75,7 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(res.StatusCode)
 	io.Copy(w, res.Body)
 	res.Body.Close()
-}	
+}
 
 func newPool(peers []string) *groupcache.HTTPPool {
 	pool := groupcache.NewHTTPPoolOpts(peers[0], nil)
@@ -62,43 +86,36 @@ func newPool(peers []string) *groupcache.HTTPPool {
 
 var group *groupcache.Group
 
-func newGroup(hostName string, cacheSizeBytes int64) {
+func newGroup(cacheSizeBytes int64) {
 	group = groupcache.NewGroup("requests", cacheSizeBytes, groupcache.GetterFunc(
-		func(_ context.Context, key string, sink groupcache.Sink) error {
+		func(ctx context.Context, key string, sink groupcache.Sink) error {
 			me, err := os.Hostname()
 			if err != nil {
 				log.Panic(err)
 			}
-	
+
 			log.Printf("Request handled by %s", me)
-	
-			// Rebuild HTTP request from cache key
-			reader := bufio.NewReader(strings.NewReader(key))
-			originalRequest, err := http.ReadRequest(reader)
-			if err != nil {
-				log.Panic(err)
+
+			// Get original request from context
+			r, ok := ctx.Value("request").(*http.Request)
+			if !ok {
+				log.Panic("Can't get original request from context")
 			}
-	
+
 			// We can't have this set on client requests
-			originalRequest.RequestURI = ""
-	
-			rawURL := "http://" + hostName
-			if originalRequest.URL.Path != "" {
-				rawURL = rawURL + originalRequest.URL.Path
-			}
-			fullUrl, err := url.Parse(rawURL)
+			r.RequestURI = ""
+			// Server requests only have the path in the URL, but client requests need the scheme + host too
+			r.URL, err= url.Parse("https://" + r.Host + r.URL.Path)
 			if err != nil {
 				log.Panic(err)
 			}
-			originalRequest.URL = fullUrl
-			originalRequest.Host = hostName
-	
+
 			client := http.Client{}
-			res, err := client.Do(originalRequest)
+ 			res, err := client.Do(r)
 			if err != nil {
 				log.Panic(err)
 			}
-	
+
 			// Write HTTP response out to bytes, store in cache
 			var outBuf bytes.Buffer
 			if err := res.Write(&outBuf); err != nil {
@@ -107,7 +124,7 @@ func newGroup(hostName string, cacheSizeBytes int64) {
 			sink.SetBytes(outBuf.Bytes(), time.Time{})
 			return nil
 		},
-	))	
+	))
 }
 
 
@@ -118,27 +135,24 @@ func main() {
 	// Default to 50mb cache
 	config.SetDefault("GROUPCACHE_SIZE_BYTES", 50000000)
 
-	proxyHostname := config.GetString("PROXY_HOSTNAME")
+	stripHeaders := config.GetString("STRIP_HEADERS")
 	cachePeers := config.GetString("GROUPCACHE_PEERS")
 	cacheSizeBytes := config.GetInt64("GROUPCACHE_SIZE_BYTES")
-	if proxyHostname == "" {
-		log.Fatal("Missing required env variable PROXY_HOSTNAME")
-	}
 	if cachePeers == "" {
 		log.Fatal("Missing required env variable GROUPCACHE_PEERS")
 	}
 
-
-
 	// Setup groupcache
-	newGroup(proxyHostname, cacheSizeBytes)
+	newGroup(cacheSizeBytes)
 	peersList := strings.Split(cachePeers, ",")
-	
 
 	log.Printf("listening on %v", peersList[0])
 	log.Printf("peers: %v", peersList)
 
-	http.HandleFunc("/", http.HandlerFunc(proxyHandler))
+	p := ProxyHandler{
+		stripHeaders: strings.Split(stripHeaders, ","),
+	} 
+	http.HandleFunc("/", http.HandlerFunc(p.Handle))
 	http.Handle("/_groupcache/", newPool(peersList))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
